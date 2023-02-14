fc6ed4c727ae7ba463e021bef7a78c55740e55f5
Nishant Dhanani <nishant.dhanani@gmail.com>
2023-02-14 16:43:25 +0000

    Subtpic practice

---
+++ Subtopics.java
@@ -0,0 +1,8 @@
+package topics;
+
+public class Subtopics {
+    public static void main(String[] args) {
+        System.out.println("Types of manual testing ");
+        System.out.println("Types of software testing");
+        System.out.println("Levels of software testing");}
+}
